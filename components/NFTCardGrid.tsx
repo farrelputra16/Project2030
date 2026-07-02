7399d2f85cee1fa1521add5cc2252d36ce048d0b
Farrel Putra <farrelkece908@gmail.com>
2026-07-02 00:10:46 -0500

    Add 3D on nft card

--- components/NFTCardGrid.tsx
+++ components/NFTCardGrid.tsx
@@ -3,7 +3,7 @@ import { BUY_STEPS, TOKEN_SYMBOL, TOKENOMICS, CA_PLACEHOLDER } from '@/lib/const
 
 const CARD_VIDEOS = [
   'https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260331_053923_22c0a6a5-313c-474c-85ff-3b50d25e944a.mp4',
-  'https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260331_054411_511c1b7a-fb2f-42ef-bf6c-32c0b1a06e79.mp4',
+  '/img/3d/htb-1.mp4',
   'https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260331_055427_ac7035b5-9f3b-4289-86fc-941b2432317d.mp4',
 ];
 
